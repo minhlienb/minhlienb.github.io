da0586dce3dbf2dabbbc613828aa152ab0b8603e
Minh Liên <96945297+minhlienb@users.noreply.github.com>
2023-09-21 17:59:07 +0000

    tdfgh

--- js/main.js
+++ js/main.js
@@ -1,26 +1,42 @@
-isSidebarToggled = 0
+// isSidebarToggled = 0
 
-let btn_sidebar = $('.sideBarClick')
-let sideBar = $('.sideBar') // Chứa đoạn side bar và cả navigation bar
+// let btn_sidebar = $('.sideBarClick')
+// let sideBar = $('.sideBar') // Chứa đoạn side bar và cả navigation bar
 
-$(document).ready(function() {
-    $('.sideBarClick').click(function() {
-        if (isSidebarToggled) {
-            $('.sideBar').removeClass('sideBarOpen1')
-            $('.sideBar').removeClass('sideBarOpen2')
-            isSidebarToggled = 0
-        } else { ///isToggled == 0
-            $('.sideBar').addClass('sideBarOpen1')
-            setTimeout(() => {
-                $('.sideBar').addClass('sideBarOpen2')
+// $(document).ready(function() {
+//     $('.sideBarClick').click(function() {
+//         if (isSidebarToggled) {
+//             $('.sideBar').removeClass('sideBarOpen1')
+//             $('.sideBar').removeClass('sideBarOpen2')
+//             isSidebarToggled = 0
+//         } else { ///isToggled == 0
+//             $('.sideBar').addClass('sideBarOpen1')
+//             setTimeout(() => {
+//                 $('.sideBar').addClass('sideBarOpen2')
                 
-            }, 200);
-            isSidebarToggled = 1
+//             }, 200);
+//             isSidebarToggled = 1
 
-        }
-    })
-})
+//         }
+//     })
+// })
 
+function RunAnim(cl) {
+    $(`.box${cl}}`).addClass('effectShadow')
+}
 
-ScrollReveal({reset: true})
-ScrollReveal().reveal('.box1', { delay: 1000 });
+function callback(cl) {
+    $(`.box${cl}`).removeClass('effectShadow')
+}
+ScrollReveal().reveal('.box4', 
+    { reset: true},
+);
+
+// ScrollReveal().watch('.box',
+//     function onEnter (el) {
+//         el.classList.add('effectShadow')
+//     },
+//     function onExit (el) {
+//         el.classList.remove('effectShadow')
+//     }
+// )
